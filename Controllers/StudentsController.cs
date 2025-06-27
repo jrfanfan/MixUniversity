414b2d3b656f81736ef44ce724ade6adcc8087f3
Jrfanfan <jrfanfan@yahoo.fr>
2025-06-27 11:51:56 -0400

    update

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -32,9 +32,13 @@ namespace MixUniversity.Controllers
             {
                 return NotFound();
             }
-
             var student = await _context.Students
+                .Include(s => s.Enrollments)
+                .ThenInclude(e => e.Course)
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
+
+            
             if (student == null)
             {
                 return NotFound();
