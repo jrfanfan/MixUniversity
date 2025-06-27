4164af8e9127206b7803bb2745da0869d1c9be27
Jrfanfan <jrfanfan@yahoo.fr>
2025-06-27 13:26:05 -0400

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MixUniversity.Data;
+using MixUniversity.Models.SchoolViewModels;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MixUniversity.Models;
@@ -7,10 +11,13 @@ namespace MixUniversity.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,6 +34,18 @@ namespace MixUniversity.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
