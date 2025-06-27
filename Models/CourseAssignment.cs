fafcadb453ee7c9b9c5761cd55f8acf3b1c7267c
Jrfanfan <jrfanfan@yahoo.fr>
2025-06-27 15:22:27 -0400

    update

---
+++ Models/CourseAssignment.cs
@@ -0,0 +1,16 @@
+﻿using MixUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MixUniversity.Models
+{
+    public class CourseAssignment
+    {
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
