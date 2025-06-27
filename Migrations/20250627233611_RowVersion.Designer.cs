573421c304b840d9b0757b9594f85c34f26cad1a
Jrfanfan <jrfanfan@yahoo.fr>
2025-06-27 19:58:25 -0400

    update

--- Migrations/20250627233611_RowVersion.Designer.cs
+++ Migrations/20250627233611_RowVersion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MixUniversity.Data;
 
@@ -11,9 +12,11 @@ using MixUniversity.Data;
 namespace MixUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20250627233611_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
