94b63a70594c732efe44bc784c5264f00ad9f20a
Jrfanfan <jrfanfan@yahoo.fr>
2025-06-27 11:26:48 -0400

    Add project files.

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using MixUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MixUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+}
